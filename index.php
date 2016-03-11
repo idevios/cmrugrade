19e2de10dc78a0be0620fe78f3bf83a3535da2f3
theerapan <theerapan_kh@ymail.com>
2016-03-11 23:36:55 +0700

    Initail

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>CMRU SHOWGRADE</title>
+</head>
+<body>
+	<br/>
+    <center><h1>OPEN FOR ALL.</h1></center>
+    <br/>
+    <br/>
+    <br/>
+    <center>
+        <form action="showgrade.php" method="POST">
+        Student Code:
+        <input type="text" name="stdcode">
+        <br/>
+        <br/>
+        <input type="submit" value="Submit">
+        </form>
+        <br/>
+        <br/>
+        <br/>
+        *Don't use for immoral.
+        <br/>
+        @Darkskystarsh1nes.
+    </center>
+    
+</body>
+</html>
---
+++ showgrade.php
@@ -0,0 +1,2 @@
+<?php $stdcode = $_POST['stdcode'];
+	header('Location: http://www.academic.cmru.ac.th/grade/transcript/id.php?login='.$stdcode); ?>
